b034b292c3a556e66b1c97f6055ec74ee8533e4b
MIZiper <miziper@163.com>
2023-06-18 12:52:37 +0200

    CounterToTacho and PulseToAzimuth

--- dac/modules/timedata/actions.py
+++ dac/modules/timedata/actions.py
@@ -72,8 +72,6 @@ class ResampleAction(ActionBase):
 class PrepDataAction(SAB, seq=[TruncAction, ResampleAction, FilterAction]): # example sequences
     ...
 
-# counter to rotational speed
-
 class ShowTimeDataAction(VAB):
     CAPTION = "Show measurement data"
     def __call__(self, channels: list[TimeData], plot_dt: float=None):
@@ -86,3 +84,36 @@ class ShowTimeDataAction(VAB):
                 ax.plot(channel.x, channel.y, label=f"{channel.name} [{channel.y_unit}]")
         
         ax.legend(loc="upper right")
+
+class CounterToTachoAction(ActionBase):
+    CAPTION = "Encoder counter to tacho"
+    def __call__(self, channel: TimeData, ppr: int=1024, sr_delta: float=0.1) -> TimeData:
+        counter = channel.y
+        delta = int(channel.fs * sr_delta)
+        rpm = np.zeros(len(counter))
+        rpm[delta:-delta] = (counter[2*delta:]-counter[:-2*delta]) / ppr / (2*delta*channel.dt / 60)
+        rpm[:delta] = rpm[delta]
+        rpm[-delta:] = rpm[-delta-1]
+
+        return TimeData(name="Tacho", y=rpm, dt=channel.dt, y_unit="rpm")
+    
+class PulseToAzimuthAction(ActionBase):
+    CAPTION = "Pulse to azimuth"
+    def __call__(self, channel: TimeData, ref_level: float, ppr: int=1, higher_as_pulse: bool=True, phase_shift: float=0) -> TimeData:
+        data = channel.y
+        inpulse = data>ref_level if higher_as_pulse else data<ref_level
+        indexes = np.arange(len(data))[inpulse]
+        idx_diff = np.diff(indexes)
+        # assert len(idx_diff) == len(indexes)-1
+        idx_pulse_end = indexes[:-1][idx_diff>np.mean(idx_diff)]
+        ang_data = np.zeros_like(data) * np.nan
+        for i, (from_idx, to_idx) in enumerate(zip(idx_pulse_end[:-1], idx_pulse_end[1:])):
+            ang_data[from_idx:to_idx] = np.arange(to_idx-from_idx)/(to_idx-from_idx)*360 + 360*i
+        
+        return TimeData(
+            name=f"Azi-{channel.name}", dt=channel.dt, y_unit="°",
+            y=(ang_data/ppr+phase_shift)%360,
+        )
+    
+class RefPulseToAzimuthAction(PAB):
+    ... # create azimuth using reference
