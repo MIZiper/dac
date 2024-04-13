ca7f9499583dd22553306e607e888688f0e1a933
MIZiper <miziper@163.com>
2024-04-13 19:33:50 +0200

    make v0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dac",
-    version="0.2.0",
+    version="0.3.0",
     description="Data action context",
     long_description="Minimal measurement data analysis with nodes and container.",
     author="MIZiper",
